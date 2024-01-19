92b5bb399f86d9f9112ee1d6d2fa9ddba1617917
Vikas Chaudhary <vikas.chaudhary@cubastion.com>
2024-01-19 10:46:16 +0530

    cross check code logic

--- src/routers/projectRouter.js
+++ src/routers/projectRouter.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const projectCtrl = require('../controllers/projectCtrl')
 const auth = require('../middleware/auth')
 
-router.post('/transaction', auth, projectCtrl.createProject)
-router.get('/transaction/:id', auth, projectCtrl.getProjectById)
-router.patch('/transaction/:id', auth, projectCtrl.updateProjectById)
-router.delete('/transaction/:id', auth, projectCtrl.deleteProjectById)
-router.get('/transaction', auth, projectCtrl.getAllProjects)
+router.post('/project', auth, projectCtrl.createProject)
+router.get('/project/:id', auth, projectCtrl.getProjectById)
+router.patch('/project/:id', auth, projectCtrl.updateProjectById)
+router.delete('/project/:id', auth, projectCtrl.deleteProjectById)
+router.get('/projects', auth, projectCtrl.getAllProjects)
+router.post('/assignProject', auth, projectCtrl.assignProject)
+router.post('/unassignProject', auth, projectCtrl.unassignProject)
+router.get('/allUsers/:id', auth, projectCtrl.getAllUsers)
+
 
 module.exports = router
